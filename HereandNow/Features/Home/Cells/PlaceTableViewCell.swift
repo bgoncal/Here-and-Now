f8d06b794a864af5ccc6eaaca353cea2de22ea10
Bruno Gonçalves <bruno.ing879@gmail.com>
2020-03-08 17:17:17 +0100

    Enhance design

--- HereandNow/Features/Home/Cells/PlaceTableViewCell.swift
+++ HereandNow/Features/Home/Cells/PlaceTableViewCell.swift
@@ -5,10 +5,12 @@ class PlaceTableViewCell: UITableViewCell {
   @IBOutlet private weak var placeName: UILabel!
   @IBOutlet private weak var openNow: UILabel!
   @IBOutlet private weak var ratingLabel: UILabel!
+  @IBOutlet weak var ratingContainer: UIView!
 
   func setup(with place: Place) {
     placeName.text = place.name
     openNow.text = place.opening_hours?.readableValue
     ratingLabel.text = place.readableRating
+    ratingContainer.roundedCorners()
   }
 }
